b46424c2e2de4e37a575f79a5a68c72fa48bf6c9
UjvariEndre <ujvendre@gmail.com>
2023-01-08 11:24:28 +0100

    Adding RestfulClient and SocketClient

---
+++ Coin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketAnalyzer
+{
+    class Coin
+    {
+        public string Ticker { get; set; }
+        public decimal LastPrice { get; set; }
+        public decimal HV30 { get; set; }
+        public List<Ohlc> Candlesticks { get; set; }
+    }
+}
--- Globals.cs
+++ Globals.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Binance.Net;
 
 namespace MarketAnalyzer
 {
@@ -10,6 +9,11 @@ namespace MarketAnalyzer
     {
         private static readonly string[] apiData;
         public static Globals Instance { get; set; }
+        public BinanceClient RestfulClient { get; private set; }
+        public BinanceSocketClient SocketClient { get; private set; }
+        public CryptoExchange.Net.Objects.WebCallResult<Binance.Net.Objects.Spot.SpotData.BinanceAccountInfo> SpotInfo { get; set; }
+        public Dictionary<string, Coin> Top200MarketCap { get; set; }
+        public string FileName { get; set; }
 
         static Globals()
         {
@@ -19,7 +23,32 @@ namespace MarketAnalyzer
 
         public async Task StartUp()
         {
-            
+            DateTime now = DateTime.Now;
+            FileName = $"{now.Year}-{now.Month.ToString("D2")}-{now.Day.ToString("D2")}";
+            //DoExcel.ReadExcel($"worksheets/{FileName}.xlsx");
+
+            RestfulClient = new BinanceClient(new Binance.Net.Objects.BinanceClientOptions()
+            {
+                // Specify options for the client
+                ApiCredentials = new CryptoExchange.Net.Authentication.ApiCredentials(apiData[0], apiData[1]),
+                AutoTimestamp = true,
+                TradeRulesBehaviour = Binance.Net.Enums.TradeRulesBehaviour.AutoComply
+            });
+
+            SocketClient = new BinanceSocketClient(new Binance.Net.Objects.BinanceSocketClientOptions()
+            {
+                // Specify options for the client
+                ApiCredentials = new CryptoExchange.Net.Authentication.ApiCredentials(apiData[0], apiData[1]),
+                AutoReconnect = true
+            });
+            try
+            {
+                SpotInfo = Instance.RestfulClient.General.GetAccountInfoAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
